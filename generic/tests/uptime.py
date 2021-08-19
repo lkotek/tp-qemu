f2dba6caf11b826bd978acff4810d10b77f2a1e6
Lukas Kotek <lkotek@redhat.com>
2021-08-19 16:54:14 +0200

    full test added

--- generic/tests/uptime.py
+++ generic/tests/uptime.py
@@ -1,7 +1,23 @@
+import logging
+
 def run(test, params, env):
+
     """
-    Docstring describing uptime.
+    Uptime test for virt guests:
+
+    1) Boot up a VM.
+    2) Establish a remote connection to it.
+    3) Run the 'uptime' command and log its results.
+
+    :param test: QEMU test object.
+    :param params: Dictionary with the test parameters.
+    :param env: Dictionary with test environment.
     """
+
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     timeout = float(params.get("login_timeout", 240))
+    session = vm.wait_for_login(timeout=timeout)
+    uptime = session.cmd("uptime")
+    logging.info("Guest uptime result is: %s", uptime)
+    session.close()
